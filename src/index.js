b25c0e719e6f7fcec542db5a7c89b8524973bb9f
Oleg <karibyon@gmail.com>
2020-09-30 20:50:29 +0300

    feat: complite array problem-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    let m = Infinity;
+    for (let number of array) {
+        if( m > number ) {
+            m = number
+        }
+        console.log(number)
+    }
+    return m;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    let m = -Infinity;
+    for (let number of array) {
+        if( m < number ) {
+            m = number
+        }
+        console.log(number)
+    }
+    return m;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    let res = 0;
+    for( let number of array) {
+        res+= number;
+    }
+    return res / array.length;
 }
